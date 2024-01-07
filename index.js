f768aad1e922e2fa7d0156cc32de6989c8d3f5d2
agrawal.abhishek <agrawal.abhishek@siemens.com>
2024-01-07 21:38:01 +0530

    initial

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+const Blog = require("./server/post");
+const mongoose = require("mongoose");
+
+console.log(process.env.MONGODB_URI);
+mongoose.connect(
+  "mongodb+srv://abhishekagr00008:Abhishek@cluster0.owotj8v.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("/news/:id", async function (req, res) {
+  try {
+    const blog = await Blog.findById(
+      new mongoose.Types.ObjectId(req.params.id)
+    );
+    if (!blog) {
+      return res.status(404).json({ error: "Blog post not found" });
+    }
+    const filePath = path.resolve(__dirname, "./build", "index.html");
+    fs.readFile(filePath, "utf8", function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      data = data.replace(/\$OG_TITLE/g, blog.title);
+      data = data.replace(/\$OG_DESCRIPTION/g, blog.subtitle);
+      data = data.replace(/\$OG_DESCRIPTION/g, blog.subtitle);
+      const result = data.replace(/\$OG_IMAGE/g, blog.imageUrl);
+      res.send(result);
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("*", function (request, response) {
+  const filePath = path.resolve(__dirname, "./build", "index.html");
+  response.sendFile(filePath);
+});
 
 app.get("/", function (req, res) {
   console.log(__dirname);
---
+++ server/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  description: String,
+  imageUrl: String,
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      name: String,
+      email: String,
+      comment: String,
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
