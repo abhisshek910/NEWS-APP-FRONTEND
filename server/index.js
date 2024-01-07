914a6bb05b36db824f47005d4d033076f8bc691a
agrawal.abhishek <agrawal.abhishek@siemens.com>
2024-01-07 15:23:35 +0530

    test

--- server/index.js
+++ server/index.js
@@ -1,35 +1,11 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
 const path = require("path");
-const fs = require("fs");
+const app = express();
 
-app.get("/", function (request, response) {
-  console.log("Home page visited!");
-  const filePath = path.resolve(__dirname, "../build", "index.html");
+app.use(express.static(path.join(__dirname, "build")));
 
-  // read in the index.html file
-  fs.readFile(filePath, "utf8", function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
-
-    // replace the special strings with server generated strings
-    data = data.replace(/\$OG_TITLE/g, "Home Page");
-    data = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
-    const result = data.replace(
-      /\$OG_IMAGE/g,
-      "https://i.imgur.com/V7irMl8.png"
-    );
-    response.send(result);
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.use(express.static(path.resolve(__dirname, "./build")));
-
-app.get("*", function (request, response) {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  response.sendFile(filePath);
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(9000);
